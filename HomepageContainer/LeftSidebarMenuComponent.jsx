1ae40cfbf21dae9ec0da42dfeff074c6b880f2d1
GodofCookery <leong.bryan.c@gmail.com>
2018-08-24 13:21:42 -0700

    leftsidebarmenu component

---
+++ HomepageContainer/LeftSidebarMenuComponent.jsx
@@ -0,0 +1,16 @@
+// Dumb component that that just links to Mens and Womens Clothes link.
+// Click on the link which redirect to either page. action to direct to new page.
+
+import React from 'react';
+
+const LeftSideBarMenuComponent = () => {
+  return (
+      <div>
+        <div>MEN'S Clothes Link</div>
+        <div>WOMEN'S Clothes Link</div>  
+      </div>
+  )    
+}
+
+
+export module LeftSideBarMenuComponent;
